4d2de28c757f289181ee9dcf87d5e6f9fef915c6
Denys Zvarychuk <denanimal@gmail.com>
2026-04-09 21:22:51 +0300

    initial commit

---
+++ src/main/java/org/island/Main.java
@@ -0,0 +1,10 @@
+package main.java.org.island;
+
+
+import main.java.org.island.app.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        new Application().run();
+    }
+}
---
+++ src/main/java/org/island/app/Application.java
@@ -0,0 +1,7 @@
+package main.java.org.island.app;
+
+public class Application {
+    public void run(){
+        System.out.println("Hello World!");
+    }
+}
