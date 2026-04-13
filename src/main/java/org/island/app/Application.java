c837641ea1bf46e26d653e08d084cf00b125a6db
Denys Zvarychuk <denanimal@gmail.com>
2026-04-14 00:24:59 +0300

    revert changes

--- src/main/java/org/island/app/Application.java
+++ src/main/java/org/island/app/Application.java
@@ -1,7 +1,16 @@
 package main.java.org.island.app;
 
+import main.java.org.island.playground.Island;
+
 public class Application {
+
+    private Island island;
+
     public void run(){
-        System.out.println("Hello World!");
+        island = new Island(10, 488567489565351L);
+        System.out.println(island.getIslandMapCLI());
+        // TODO generate animals
+        // TODO split island to tiles 10x10 or something
+        // TODO behaviour tasks on Tiles
     }
 }
