6ef676e6b9ae6aad45fb58d2a5264bdb3e9578df
Den Zvarychuk <denanimal@gmail.com>
2026-04-17 20:35:22 +0300

    initial comit to feature/entity

--- src/main/java/org/island/entity/Entity.java
+++ src/main/java/org/island/entity/Entity.java
@@ -2,7 +2,7 @@ package org.island.entity;
 
 public abstract class Entity {
 
-    private boolean isAlive;
-    private double health;
-    private int id;
+    private String id;
+    private boolean isExist;
+
 }
